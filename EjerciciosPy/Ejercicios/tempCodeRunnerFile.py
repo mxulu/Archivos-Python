ed5040ee51b7a464db973b0f5b33e5e45bea8f91
Mauricio Xulú <angelmxju@gmail.com>
2022-02-17 21:15:17 -0600

    g

--- EjerciciosPy/Ejercicios/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-
-NombrePersona = input("Por favor ingrese su nombre:")
-print("Hola, te envío un saludo", NombrePersona, "en la fecha:", datetime.today().strftime("%y-%m-%d"))
